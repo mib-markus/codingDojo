8f4dcf585eea86f1d0145a6df29da7dbced4ccc9
markus.beck1 <markus.beck1@allianzdirect.de>
2021-05-20 14:32:59 +0200

    initial commit Caveman Dojo

---
+++ src/main/kotlin/de/allianzdirect/codingdojo/CodingdojoApplication.kt
@@ -0,0 +1,11 @@
+package de.allianzdirect.codingdojo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodingdojoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodingdojoApplication>(*args)
+}
